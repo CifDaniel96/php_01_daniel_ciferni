19625c9eca19d413c5da30ee20506e1d32d9e5b0
Daniel Ciferni <cif.daniel96@gmail.com>
2025-11-03 18:28:02 +0100

    Riorganizzata la struttura delle tracce

---
+++ traccia1/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$integer = 19;
+$float = 9.5;
+$string = "Ciao";
+$booleano = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($booleano);
+
+// Trasformo le variabili in costanti
+
+const INTERO = 19;
+const DECIMALE = 9.5;
+const TESTO = "Ciao";
+const VERO_FALSO = true;
+
+var_dump(INTERO);
+var_dump(DECIMALE);
+var_dump(TESTO);    
+var_dump(VERO_FALSO);
